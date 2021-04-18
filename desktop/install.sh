b938d51b8e54ea71e6e9ec26243c4ff066cc8822
jachin <ygcaicn@gmail.com>
2021-04-18 08:07:41 +0800

    fix

--- desktop/install.sh
+++ desktop/install.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
+#bash -c "$(curl -L https://git.io/JORao)" @ tim TIM
+
 # arg1: script name
 # arg2: descripe name
-install(){
+install_desktop(){
     if ! [ -x "$(command -v docker)" ]; then
         echo 'Error: docker is not installed.' >&2
         exit 1
     fi
-    [ -n ~/.local/bin/ ] && mkdir -p ~/.local/bin/
-    p=$(grep ~/.local/bin: ~/.bashrc)
-    [ -n $p ] && echo "export PATH=\"$HOME/.local/bin:\$PATH\"" >> ~/.bashrc && source ~/.bashrc
-    [ -n ~/.local/share/icons/hicolor/256x256/apps ] && mkdir -p ~/.local/share/icons/hicolor/256x256/apps
+    [ -n ${HOME}/.local/bin/ ] && mkdir -p ${HOME}/.local/bin/
+    [ -n ${HOME}/.local/share/icons/hicolor/256x256/apps ] && mkdir -p ${HOME}/.local/share/icons/hicolor/256x256/apps
 
+    if ! [ -x "${HOME}/.local/bin/${1}.sh" ]; then
+        echo "Install this script to ${HOME}/.local/bin/${1}.sh"
 
-    if ! [ -x "~/.local/bin/${1}.sh" ]; then
-        echo "Install this script to ~/.local/bin/${1}.sh" >&2
-        curl -L -H "Cache-Control: no-cache" https://raw.githubusercontent.com/ygcaicn/docker-deepin/main/${1}.sh \
-        -o "~/.local/bin/$1.sh"
-        chmod +x "~/.local/bin/$1.sh"
-        ln -i "~/.local/bin/$1.sh" "~/.local/bin/$1"
-        cmd="/home/$(whoami)/.local/bin/$1.sh"
-        curl -L -H "Cache-Control: no-cache" https://raw.githubusercontent.com/ygcaicn/ubuntu_qq/master/$1.png \
-        -o ~/.local/share/icons/hicolor/256x256/apps/$1.png
+        curl -Ls -H "Cache-Control: no-cache" "https://raw.githubusercontent.com/ygcaicn/docker-deepin/main/desktop/$1.sh" -o "${HOME}/.local/bin/$1.sh"
+        chmod +x "${HOME}/.local/bin/$1.sh"
+        ln -i "${HOME}/.local/bin/$1.sh" "${HOME}/.local/bin/$1"
+        cmd="${HOME}/.local/bin/$1.sh"
+        curl -Ls -H "Cache-Control: no-cache" https://raw.githubusercontent.com/ygcaicn/docker-deepin/main/desktop/$1.png \
+        -o ${HOME}/.local/share/icons/hicolor/256x256/apps/$1.png
+        ls ${HOME}/.local/share/icons/hicolor/256x256/apps/$1.png
         cat <<-EOF > /home/$(whoami)/.local/share/applications/$1.desktop
 [Desktop Entry]
 Categories=Network;InstantMessaging;
@@ -36,9 +36,9 @@ Name[en_US]=$2
 EOF
 
     else
-        echo "already installed at ~/.local/bin/$1.sh"
+        echo "already installed at ${HOME}/.local/bin/$1.sh"
     fi
   return 0
 }
 
-install $*
+install_desktop $*
